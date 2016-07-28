f8912d3d7f25d47bfee53616f8b3a1e159558cb5
LinQuan <i@linquan.name>
2016-07-28 22:47:33 +0800

    extend

--- routerJS.js
+++ routerJS.js
@@ -55,6 +55,10 @@ console.time('123');
         js: []
     };
 
+    var getProto = Object.getPrototypeOf;
+    var fnToString = hasOwn.toString;
+    var ObjectFunctionString = fnToString.call( Object );
+
     function isRemote(path) {
         var _link_tmp = document.createElement('a');
         _link_tmp.href = path;
@@ -139,6 +143,98 @@ console.time('123');
             });
         }
         return target;
+    }
+
+    function extend() {
+        var options, name, src, copy, copyIsArray, clone,
+            target = arguments[ 0 ] || {},
+            i = 1,
+            length = arguments.length,
+            deep = false;
+
+        // Handle a deep copy situation
+        if ( typeof target === "boolean" ) {
+            deep = target;
+
+            // Skip the boolean and the target
+            target = arguments[ i ] || {};
+            i++;
+        }
+
+        // Handle case when target is a string or something (possible in deep copy)
+        if ( typeof target !== "object" && !isFunction( target ) ) {
+            target = {};
+        }
+
+        // Extend jQuery itself if only one argument is passed
+        if ( i === length ) {
+            target = this;
+            i--;
+        }
+
+        for ( ; i < length; i++ ) {
+
+            // Only deal with non-null/undefined values
+            if ( ( options = arguments[ i ] ) != null ) {
+
+                // Extend the base object
+                for ( name in options ) {
+                    src = target[ name ];
+                    copy = options[ name ];
+
+                    // Prevent never-ending loop
+                    if ( target === copy ) {
+                        continue;
+                    }
+
+                    // Recurse if we're merging plain objects or arrays
+                    if ( deep && copy && ( isPlainObject( copy ) ||
+                        ( copyIsArray = isArray( copy ) ) ) ) {
+
+                        if ( copyIsArray ) {
+                            copyIsArray = false;
+                            clone = src && isArray( src ) ? src : [];
+
+                        } else {
+                            clone = src && isPlainObject( src ) ? src : {};
+                        }
+
+                        // Never move original objects, clone them
+                        target[ name ] = extend( deep, clone, copy );
+
+                    // Don't bring in undefined values
+                    } else if ( copy !== undefined ) {
+                        target[ name ] = copy;
+                    }
+                }
+            }
+        }
+
+        // Return the modified object
+        return target;
+    }
+
+    console.log(extend({}, {a: "b"}));
+
+    function isPlainObject( obj ) {
+        var proto, Ctor;
+
+        // Detect obvious negatives
+        // Use toString instead of jQuery.type to catch host objects
+        if ( !obj || !isObject( obj ) ) {
+            return false;
+        }
+
+        proto = getProto( obj );
+
+        // Objects with no prototype (e.g., `Object.create( null )`) are plain
+        if ( !proto ) {
+            return true;
+        }
+
+        // Objects with prototype are plain iff they were constructed by a global Object function
+        Ctor = hasOwn.call( proto, "constructor" ) && proto.constructor;
+        return typeof Ctor === "function" && fnToString.call( Ctor ) === ObjectFunctionString;
     }
 
     /**
